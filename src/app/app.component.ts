e4844a29a3aeea9a27b25148647429cc2fa43d47
rajendra-redorange <rajendra.kandpal@redorangetechnologies.com>
2021-08-31 15:21:28 +0530

    image library integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,  ElementRef} from '@angular/core';
+import { LibTestRkService } from 'lib-test-rk';
+import { ImageFile } from 'lib-test-rk/lib/models/imageFile.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lib-consumer';
+  imgFile: ImageFile;
+  isLoading: boolean;
+  rotated = false;
+  @ViewChild('rkImageElement') imageElement: ElementRef<HTMLImageElement>;
+
+  constructor(private imgService: LibTestRkService) {}
+
+  readStart(started: boolean): void {
+    this.isLoading = true;
+  }
+
+  readComplete(imgFile: ImageFile): void {
+    this.imgFile = imgFile;
+    this.isLoading = false;
+  }
+
+  removeFile(index): void {
+    this.isLoading = true;
+    this.imgService.removeFile(index, this.imgFile);
+    this.isLoading = false;
+  }
+
+  rotate(): void {
+    this.imgService.rotateImage(this.imageElement, '180');
+    this.rotated = true;
+  }
+
+  clearRotation(): void {
+    this.imgService.rotateImage(this.imageElement, '0');
+    this.rotated = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LibTestRkModule } from 'lib-test-rk';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, LibTestRkModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
